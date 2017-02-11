e5816927454e1266e05f5a8e94406e150684d3e3
qdhk <15230277836@163.com>
2017-02-11 20:27:39 +0800

    Handle JSON and XML 处理 JSON 和 XML

--- src/com/nr/demo/Hello.java
+++ src/com/nr/demo/Hello.java
@@ -1,5 +1,7 @@
 package com.nr.demo;
 
+import com.nr.demo.bean.User;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,10 @@ import javax.ws.rs.core.MediaType;
 /**
  * Created by nr on 2017/02/10 0010.
  */
-@Path("/hello")
+@Path("/webapi/myresource")
 public class Hello {
     // This method is called if TEXT_PLAIN is request
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String sayPlainTextHello(){
@@ -31,4 +34,33 @@ public class Hello {
                 + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
     }
 
+    /**
+     * 方法处理 HTTP GET 请求。返回的对象以"application/json"媒体类型
+     * 给客户端
+     * @return User 以 application/json 形式响应
+     */
+    @GET
+    @Path("/userJson")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUserJson(){
+        User user=new User();
+        user.setName("niurui");
+        user.setAge(23);
+        return user;
+    }
+
+    /**
+     * 方法处理 HTTP GET 请求。返回的对象以"application/xml"媒体类型
+     * 给客户端
+     * @return User 以 application/xml 形式响应
+     */
+    @GET
+    @Path("/userXml")
+    @Produces(MediaType.APPLICATION_XML)
+    public User getUserXml(){
+        User user=new User();
+        user.setName("niurui");
+        user.setAge(23);
+        return user;
+    }
 }
